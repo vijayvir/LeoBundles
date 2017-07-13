22f886a4cdef03818c37af16315069dd17c32955
vijay vir <virvijay37@gmail.com>
2017-07-13 13:20:23 +0530

    added marks
    added marks

--- SwiftFiles/LeoReadJson.swift
+++ SwiftFiles/LeoReadJson.swift
@@ -10,14 +10,30 @@ import Foundation
 
 
 
+
+/**
+
+
+This functions read the files and return the Key: Values Array
+
+- parameter bunldePath:  name of the file.
+
+- parameter extention: files extentions .
+
+- returns  : [String: Any]
+
+	# Example :
+	       let response = leoReadJson(bunldePath: "ReservedJobApiResponse", extention: "json")
+
+- Author: Vijavir
+*/
 public func leoReadJson(bunldePath : String , extention  : String)  ->  [String: Any] {
-
-	if let path = Bundle.main.path(forResource: bunldePath, ofType: extention ), let dict = NSDictionary(contentsOfFile: path) as? [String: Any] {
 	
+	if let path = Bundle.main.path(forResource: bunldePath, ofType: extention ), let dict = NSDictionary(contentsOfFile: path) as? [String: Any] {
+		
 		return dict
-
+		
 	}
 	
 	return [:]
 }
-
